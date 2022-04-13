d83d95acc06c86ee6c56e9f3b5a356e56efb41ef
manpriya-kaur <manpriya@chicagobooth.edu>
2022-04-13 00:52:49 +0000

    Float assignment

--- float_find_hypotenuse.rb
+++ float_find_hypotenuse.rb
@@ -1,7 +1,10 @@
 # A right triangle's two shorter sides have length 2.8 and 4.5. How long is its longest side?
-# 
+#
 # Your output should be a number; for example,
-# 
+#
 # 5.3
 
-
+a = 2.8
+b = 4.5
+result = Math.sqrt(a ** 2 + b ** 2)
+p result
--- float_round.rb
+++ float_round.rb
@@ -1,2 +1,5 @@
 # Output ten divided by 3 rounded to 3 decimal places.
-  
+
+a = 10 / 3.to_f
+result = a.round(3)
+p result
